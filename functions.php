109e45158fd775176a149e610f6f571b5e008e96
erikkowalski <erikkowalski@mac.com>
2015-07-29 09:56:59 -0400

    added bootstrap nav walker to project

--- functions.php
+++ functions.php
@@ -18,6 +18,8 @@ $sage_includes = [
   'lib/assets.php',                // Scripts and stylesheets
   'lib/titles.php',                // Page titles
   'lib/extras.php',                // Custom functions
+  'lib/wp_bootstrap_navwalker.php',  // Bootstrap Nav walker
+
 ];
 
 foreach ($sage_includes as $file) {
--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,24 @@
-<header class="banner" role="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><img class="visible-xs visible-sm" src="<?php bloginfo('template_directory'); ?>/dist/images/psh-web-sm.png" alt=""><img class="hidden-xs hidden-sm" src="<?php bloginfo('template_directory'); ?>/dist/images/psh-web.png" alt=""> <span class="sr-only"><?php bloginfo('name'); ?></span></a>
+			<p class="visible-xs mobil-number">1&#40;800&#41;350-4774</p>
+		</div>
+
+		<nav class="collapse navbar-collapse navbar-right" role="navigation">
+			<?php
+if (has_nav_menu('primary_navigation')) :
+wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
+endif;
+			?>
+		</nav>
+	</div>
 </header>
+
+
