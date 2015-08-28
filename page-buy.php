2f01d00db2a2ab3093d0177c751a4533a0b630a2
erikkowalski <erikkowalski@mac.com>
2015-08-28 19:23:29 -0400

    Added position styles to buy page

--- page-buy.php
+++ page-buy.php
@@ -1,8 +1,13 @@
 <?php while (have_posts()) : the_post(); ?>
 <?php // get_template_part('templates/page', 'header'); ?>
-<aside class="buy-search">
-	<img class="img-responsive" src="<?php the_field("buy_page_header_img"); ?>" alt="">
+
+<section class="buy-search">
+	<div class="buy-header">
+		<img class="img-responsive" src="<?php the_field("buy_page_header_img"); ?>" alt="luxury kitchen">
+	</div>
+<aside class="search-box">
 	<?php the_field('buy_page_search'); ?>
 </aside>
+</section>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
