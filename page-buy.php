5ccc284c49315f8d5138d7a4f8b691912ee57352
erikkowalski <erikkowalski@mac.com>
2015-08-28 18:31:33 -0400

    Added padding to buy search area

--- page-buy.php
+++ page-buy.php
@@ -1,4 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
 <?php // get_template_part('templates/page', 'header'); ?>
+<aside class="buy-search">
+	<?php the_field('buy_page_search'); ?>
+</aside>
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
